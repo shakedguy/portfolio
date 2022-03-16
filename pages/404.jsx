37c90ed21abac3d80667fed243050dc28c45c6bc
shakedguy <shakedguy94@gmail.com>
2022-03-16 17:14:32 +0200

    update

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import { useRouter } from 'next/router';
 import Layout from '../contexts/Layout';
-import { Heading, Center, Text, Icon, Button, VStack } from '@chakra-ui/react';
+import { Heading, Center, Text, Icon, Button, VStack, Img, Flex } from '@chakra-ui/react';
 import { IoHomeOutline } from 'react-icons/io5';
 import containerClasses from '../styles/Container.module.css';
 
@@ -13,7 +13,34 @@ const NotFoundPage = () => {
   const style = isDarkMode ? containerClasses.contactStackDark : containerClasses.contactStackLight;
   return (
     <Center px={1} my={2}>
-      <VStack className={style} boxShadow={'dark-lg'}>
+      <Flex dir='row'>
+        <VStack className={style} boxShadow={'dark-lg'}>
+          <Heading color={'#222'} py={10} as='h2' size={'lg'}>
+            we can't seem to find the page you're looking for.
+          </Heading>
+          <Text color={'#222'} fontSize={'1xl'}>
+            To return to the home page, click
+          </Text>
+          <Button
+            onClick={() => router.replace('/')}
+            variant={'outline'}
+            _hover={{ bg: 'gray.300' }}
+            color={'gray.900'}
+            borderColor={'gray.900'}
+            my={10}
+            leftIcon={<Icon as={IoHomeOutline} />}>
+            Home
+          </Button>
+        </VStack>
+
+        <Img
+          bg={'transparent'}
+          h={'25.6rem'}
+          src='https://a0.muscache.com/airbnb/static/error_pages/404-Airbnb_final-d652ff855b1335dd3eedc3baa8dc8b69.gif'
+        />
+      </Flex>
+
+      {/* <VStack className={style} boxShadow={'dark-lg'}>
         <Heading color={'#222'} py={10} as='h2' size={'lg'}>
           Sorry, the page was not found...
         </Heading>
@@ -30,7 +57,7 @@ const NotFoundPage = () => {
           leftIcon={<Icon as={IoHomeOutline} />}>
           Home
         </Button>
-      </VStack>
+      </VStack> */}
     </Center>
   );
 };
