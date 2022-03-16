7aa9a082c57e2f11f76e4c25642d3481a21b27b7
shakedguy <shakedguy94@gmail.com>
2022-03-16 18:11:08 +0200

    update

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -36,7 +36,7 @@ const Footer = () => {
           icons.map((icon, index) => (
             <Tooltip placement='top-start' key={index} hasArrow label={data.tooltips[index]}>
               <Link
-                href={index === 3 ? path.join(process.cwd(), 'resources', data.links[index]) : data.links[index]}
+                href={index === 3 ? `/${data.links[index]}` : data.links[index]}
                 target={'_blank'}
                 download={index === 3 ? data.links[index] : null}
                 passHref>
@@ -50,30 +50,3 @@ const Footer = () => {
 };
 
 export default Footer;
-
-{
-  /* <Container
-  bg={'rgba(230, 230, 230, 0.3)'}
-  maxW={'100vw'}
-  position={'fixed'}
-  w={'100vw'}
-  h={35}
-  bottom={0}
-  m={0}
-  p={0}>
-  <Flex position={'fixed'} right={2} bottom={1} h={25}>
-    <Center position={'fixed'} left={'45%'}>
-      <Icon as={CgCopyright} mx={2} w={5} h={5} />
-      <Text fontSize={'sm'} fontFamily={'Raleway'}>
-        Created by Guy Shaked
-      </Text>
-    </Center>
-    <Icon as={BsTelephoneFill} color={'teal.600'} mx={2} w={5} h={5} />
-    <Icon as={SiWaze} color={'blue.700'} mx={2} w={5} h={5} />
-    <Icon as={SiLinkedin} color={'blue.700'} mx={2} w={5} h={5} />
-    <Icon as={FcGoogle} mx={2} w={5} h={5} />
-    <Icon as={SiGithub} color={'purple.700'} mx={2} w={5} h={5} />
-    <Icon as={SiWhatsapp} color={'teal.600'} mx={2} w={5} h={5} />
-  </Flex>
-</Container>; */
-}
