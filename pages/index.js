54a20925ab44a2e157c66d21b17e5eaa3a9493d6
shakedguy <shakedguy94@gmail.com>
2022-03-16 16:23:33 +0200

    update

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,13 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext } from 'react';
 import TypeWriter from './../components/TypeWriter';
 import Layout from '../contexts/Layout';
 import { Box } from '@chakra-ui/react';
-import useSWR from 'swr';
-import { fetcher } from '../services/clientHelpers';
+
+const lines = ['hello, my name is Guy Shaked,', 'and i am a computer science student'];
 
 const Home = () => {
-  const { data, error } = useSWR('/api/home', fetcher);
-  const [lines, setLines] = useState(null);
   const layoutCtx = useContext(Layout);
   layoutCtx.setPageName('Home');
-
-  useEffect(() => {
-    if (data) {
-      setLines(data.lines);
-    }
-  }, [data]);
 
   return (
     <Box className='page' flex={1} alignSelf={'start'} justifySelf={'left'}>
